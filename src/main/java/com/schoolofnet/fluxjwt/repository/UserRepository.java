c8555b7d6c9c543fde6f6b55459264ff06d6c6c8
Eliel Batiston <batistondeoliveira@yahoo.com.br>
2023-12-13 00:14:44 -0300

    add findByUsername

--- src/main/java/com/schoolofnet/fluxjwt/repository/UserRepository.java
+++ src/main/java/com/schoolofnet/fluxjwt/repository/UserRepository.java
@@ -2,5 +2,9 @@ package com.schoolofnet.fluxjwt.repository;
 
 import com.schoolofnet.fluxjwt.model.User;
 import org.springframework.data.mongodb.repository.ReactiveMongoRepository;
+import reactor.core.publisher.Mono;
 
-public interface UserRepository extends ReactiveMongoRepository<User, String> { }
+public interface UserRepository extends ReactiveMongoRepository<User, String> {
+
+	Mono<User> findByUsername(String username);
+}
