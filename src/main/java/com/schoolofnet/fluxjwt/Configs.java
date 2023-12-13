00ef5ec9c30d4a4cad96bd30fbf70de33d238922
Eliel Batiston <batistondeoliveira@yahoo.com.br>
2023-12-12 22:52:59 -0300

    add configs

---
+++ src/main/java/com/schoolofnet/fluxjwt/Configs.java
@@ -0,0 +1,23 @@
+package com.schoolofnet.fluxjwt;
+
+import com.schoolofnet.fluxjwt.handler.AuthHandler;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.MediaType;
+
+import org.springframework.web.reactive.function.server.RouterFunction;
+import org.springframework.web.reactive.function.server.ServerResponse;
+
+import static org.springframework.web.reactive.function.server.RequestPredicates.GET;
+import static org.springframework.web.reactive.function.server.RequestPredicates.POST;
+import static org.springframework.web.reactive.function.server.RequestPredicates.accept;
+import static org.springframework.web.reactive.function.server.RouterFunctions.route;
+
+@Configuration
+public class Configs {
+
+	@Bean
+	public RouterFunction<ServerResponse> auth(final AuthHandler handler) {
+		return route(GET("/sign-up").and(accept(MediaType.APPLICATION_JSON)), handler::signUp);
+	}
+}
