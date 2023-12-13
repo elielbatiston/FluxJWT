125b0f27ec712b73836a1e40995b5f1c63a9ce1a
Eliel Batiston <batistondeoliveira@yahoo.com.br>
2023-12-12 23:12:52 -0300

    change handler

--- src/main/java/com/schoolofnet/fluxjwt/handler/AuthHandler.java
+++ src/main/java/com/schoolofnet/fluxjwt/handler/AuthHandler.java
@@ -2,6 +2,7 @@ package com.schoolofnet.fluxjwt.handler;
 
 import com.schoolofnet.fluxjwt.model.User;
 import com.schoolofnet.fluxjwt.repository.UserRepository;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.web.reactive.function.BodyInserters;
 import org.springframework.web.reactive.function.server.ServerRequest;
@@ -13,14 +14,20 @@ public class AuthHandler {
 
 	private final UserRepository repository;
 
-	public AuthHandler(final UserRepository repository) {
+	private final BCryptPasswordEncoder encoder;
+
+	public AuthHandler(final UserRepository repository, final BCryptPasswordEncoder encoder) {
 		this.repository = repository;
+		this.encoder = encoder;
 	}
 
 	public Mono<ServerResponse> signUp(final ServerRequest req) {
 		final Mono<User> userMono = req.bodyToMono(User.class);
-		return userMono.map(u -> new User(u.getUsername(), u.getPassword()))
+		return userMono.map(u -> {
+				final String password = encoder.encode(u.getPassword());
+				return new User(u.getUsername(), password);
+			})
 			.flatMap(this.repository::save)
-			.flatMap(user -> ServerResponse.ok().body(user, User.class));
+			.flatMap(user -> ServerResponse.ok().body(BodyInserters.fromValue(user)));
 	}
 }
